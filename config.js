b89c8a67000103ca698dc5e5600d06de81a75353
jutt369 <moizjutt63726@gmail.com>
2026-01-20 16:42:33 +0500

    Update session

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ const defaults = {
 
 let cache = {}
 
-const SESSION_ID = process.env.SESSION_ID || ''
+const SESSION_ID = process.env.SESSION_ID || 'DAVE-S*F=SS4T83CDZM'
 cache.SESSION_ID = SESSION_ID 
 
 async function initConfig() {
